2c348644f21f79570d284ef25e8f67aa8f9b57d4
codershona <falgunimst95@gmail.com>
2020-03-02 15:05:45 +0100

    adding functions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector('.visual');
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#6860d3",
+    "#60b2d3"
+  ];
 
   // console.log(sounds[0]);
 
@@ -8,10 +17,19 @@ window.addEventListener("load", () => {
 
   pads.forEach((pad, index) => {
     pad.addEventListener('click', function(){
+      sounds[index].currentTime = 0;
      // sounds[4]
      sounds[index].play();
     });
   });
+
+  // create a function that makes bubbles
+
+  const createBubbles = (index) => {
+    const bubble = document.createElement("div");
+    visual .appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+  }
 });
 
 // console.log(sounds);
