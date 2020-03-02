c0ff775462cae00bed85ee492cec0fd1383e96d2
codershona <falgunimst95@gmail.com>
2020-03-02 14:45:34 +0100

    console have no erroe , NodeList is working

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+
+  console.log(sounds);
+
+  // lets get going with the sound here :
+
+  pads.forEach(pad => {
+    pad.addEventListener('click', function(){
+     sounds[4]
+    });
+  });
 });
+
+// console.log(sounds);
